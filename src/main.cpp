922a723d079434f01e3a621e56f82075f581b2b8
jessiehunt <150263710+jessiehunt@users.noreply.github.com>
2023-11-22 19:37:03 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,21 +13,18 @@ void loop() {
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
-int greenLED=2;
-int redLED=3;
-int speaker=4;
-int phresPin=12;
-int photores=A0; 
+#define Green_LED 2
+#define Red_LED 3
+#define Speaker 4
+#define phresPin 12
+#define photores A0 
+#define buttonPin 5
+#define solenoidPin 8 
+int buttonVoltage;
 int prVoltage;
 int thresholddark = 100;
 int thresholdmedium = 300;
 int thresholdbright=750;
-int buttonPin = 5;
-int solenoidPin = 8; 
-int buttonVoltage;
 
 
 void setup() {
@@ -41,22 +38,19 @@ void setup() {
 }
 
 void loop() {
- buttonVoltageGreen= digitalRead(buttonPin);
+ buttonVoltage= digitalRead(buttonPin);
   if(buttonVoltage == LOW) {
       digitalWrite(solenoidPin, LOW);//make sure door isnt deadbolted 
       digitalWrite(greenLED, HIGH);//turn on LED to indicate the system is on 
     //laser movement and such  
-    if //laser is broken
+    //if laser is broken
       digitalWrite(solenoidPin, HIGH);      //Switch Solenoid ON
       digitalWrite(greenLED, LOW);
       digitalWrite (redLED, HIGH);
       tone (speaker, 400, 300);
-       digitalWrite (redLED, LOW);
-       tone (speaker, 400, 300);
-      if (buttonVoltage==LOW){
-        digitalWrite(redLED, LOW);
-        digitalWrite(solenoidPin, LOW);
-        break 
+      digitalWrite (redLED, LOW);
+      tone (speaker, 400, 300);
+    
       }
     else{
       delay(100);
